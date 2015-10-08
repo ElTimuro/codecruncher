a2c9a18c3574cfd51e598b6a2ce514e697eebca0
unknown <Administrator@SP2013BASE.SP2013DEV.local>
2015-10-08 13:37:13 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodeCruncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+            var header = "***********************************" + Environment.NewLine;
+
+            var files = Directory.GetFiles(rootPath, "*.cs", SearchOption.AllDirectories);
+
+            var result = files.Select(path => new { Name = Path.GetFileName(path), Contents = File.ReadAllText(path) })
+                              .Select(info =>
+                                  header
+                                + "Filename: " + info.Name + Environment.NewLine
+                                + header
+                                + info.Contents);
+
+
+            var singleStr = string.Join(Environment.NewLine, result);
+            //Console.WriteLine(singleStr);
+            Console.WriteLine("Crunching...");
+            var p = AppDomain.CurrentDomain.BaseDirectory + @"Crunched.cs";
+            File.WriteAllText(p, singleStr, Encoding.UTF8);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
